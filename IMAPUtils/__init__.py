331351e03935879ac4b9f6f6b55688576b780c1d
Michal Čihař <michal@cihar.com>
2007-05-22 09:00:00 +0000

    Increase version.

--- IMAPUtils/__init__.py
+++ IMAPUtils/__init__.py
@@ -22,7 +22,7 @@ this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 import sys
 import imaplib
