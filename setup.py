bb0a9f4aebb7dc16a45fa30441bc40fcd3562923
Michal Čihař <michal@cihar.com>
2015-10-01 10:13:04 +0200

    Switch to setuptools
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import distutils.core
+from setuptools import setup
 import IMAPUtils
 
-distutils.core.setup(
+setup(
     name="imap-utils",
     version=IMAPUtils.__version__,
     description="IMAP utils",
